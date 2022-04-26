8cbf9bbfc185d56f2544c2b5eef139c641c91067
Bradley Clifton <darb.clifton@gmail.com>
2022-04-26 16:59:42 -0400

    Implement function that plays a single round

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -4,10 +4,41 @@ function computerPlay() {
     let move = Math.floor(Math.random()*3) + 1;
     switch(move) {
         case 1:
-            return 'Rock';
+            return 'rock';
         case 2:
-            return 'Paper';
+            return 'paper';
         case 3:
-            return 'Scissors';
+            return 'scissors';
+    }
+}
+
+//take user play and computer play as inputs
+//make them case insensitive using .toLowerCase()
+//compare the two moves using if else statements
+//return whether the user wins or loses and what the two moves were
+
+function singleRound(userPlay, computerPlay) {
+    userPlay = userPlay.toLowerCase();
+    if(userPlay === computerPlay) return `It's a Draw! ${userPlay} ties ${computerPlay}`
+
+    if(userPlay === 'rock' && computerPlay === 'scissors') {
+        return `You Win! Rock beats Scissors`;
+    }
+    else {
+        return `You Lose! Paper beats Rock`;
+    }
+
+    if(userPlay === 'paper' && computerPlay === 'rock') {
+        return `You Win! Paper beats Rock`;
+    }
+    else {
+        return `You Lose! Scissors beats Paper`;
+    }
+
+    if(userPlay === 'scissors' && computerPlay === 'paper') {
+        return `You Win! Scissors beats Paper`;
+    }
+    else {
+        return `You Lose! Rock beats Scissors`;
     }
 }
